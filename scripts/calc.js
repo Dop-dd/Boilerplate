41a37466a97d561b4e703797be4953f352a0dbc9
Dop-dd <dopdd@yahoo.com>
2020-05-14 05:42:26 +0000

    Refactoring- breaking our test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,24 @@
-function addition(num1, num2) {
+// creating a prototype in case of changes in code.
+
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) { // function take s one number as an argument
+    if (typeof(numner) == "number") {
+        this.value += number;        
+    } else {
+        alert("Error")
+    }
+}
+
+
+
+/*function addition(num1, num2) {
     if (typeof(num1) == "number" && typeof(num2) == "number") {
     return num1 + num2;
     } else {
         alert("Error");
-    }
-   
+    }   
 }
+*/
