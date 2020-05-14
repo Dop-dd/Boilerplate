9f24defca5ec6433ca6b9109625b0b9920686d18
Dop-dd <dopdd@yahoo.com>
2020-05-14 05:30:44 +0000

    Jasmine Spies

--- scripts/calc.js
+++ scripts/calc.js
@@ -2,7 +2,7 @@ function addition(num1, num2) {
     if (typeof(num1) == "number" && typeof(num2) == "number") {
     return num1 + num2;
     } else {
-        return "Error";
+        alert("Error");
     }
    
 }
